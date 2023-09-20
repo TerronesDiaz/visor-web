ae79e438ed8dfb59098870bf4eb0e8a5fff4fa9d
TERRONES DIAZ FRANCISCO JAVIER <fterrones@ucol.mx>
2023-09-19 23:13:46 -0600

    Primer commit

---
+++ apiCajon/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const cajonRoutes = require('./routes/cajon');
+
+const app = express();
+const port = 3000;
+
+// Habilitar CORS
+app.use(cors());
+
+app.use('/cajon', cajonRoutes);
+
+app.listen(port, () => {
+  console.log(`API escuchando en http://localhost:${port}`);
+});
+
+
+//electron-packager . MiAplicacion --platform=win32 --arch=x64 --out=./ --overwrite
---
+++ apiCajon/routes/cajon.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+// Importa cualquier librería para interactuar con USB si es necesario
+
+router.post('/abrir', (req, res) => {
+  try {
+    // Aquí el código para abrir el cajón
+    res.json({ error: false, mensaje: 'Cajón abierto con éxito' });
+  } catch (err) {
+    res.json({ error: true, mensaje: 'Error al abrir el cajón' });
+  }
+});
+
+module.exports = router;
+
+
+
+//electron-packager . MiAplicacion --platform=win32 --arch=x64 --out=./ --overwrite
